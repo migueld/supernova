3c4b33d977b646af64d966e96b3f9d24c26f48db
Faustino Osuna <enrique.osuna@gmail.com>
2008-06-18 01:22:34 +0100

    Really get rid of xTV.ttf and use constants referenced in xbmcgui python module.

--- extras/MediaModuleUtils.py
+++ extras/MediaModuleUtils.py
@@ -6,23 +6,6 @@ import traceback
 import xbmcgui
 import xbmc
 
-ACTION_MOVE_LEFT              = 1  
-ACTION_MOVE_RIGHT             = 2
-ACTION_MOVE_UP                = 3
-ACTION_MOVE_DOWN              = 4
-ACTION_PAGE_UP                = 5 
-ACTION_PAGE_DOWN              = 6
-ACTION_SELECT_ITEM            = 7
-ACTION_HIGHLIGHT_ITEM         = 8
-ACTION_PARENT_DIR             = 9
-ACTION_PREVIOUS_MENU          = 10
-ACTION_SHOW_INFO              = 11
-ACTION_PAUSE                  = 12
-ACTION_STOP                   = 13
-ACTION_NEXT_ITEM              = 14
-ACTION_PREV_ITEM              = 15
-ACTION_CONTEXT_MENU           = 117
-
 CONTROL_MODULE_TITLE          = 4001
 CONTROL_MAIN_LIST             = 4000
 CONTROL_BREADCRUMB_POSITION_0 = 3000
@@ -38,7 +21,7 @@ class MediaModuleWindow(xbmcgui.WindowXML):
         self.mediaType = sys.argv[1]
         
     def onAction(self, action):
-        if action.getId() == ACTION_PREVIOUS_MENU:
+        if action.getId() == xbmcgui.ACTION_PREVIOUS_MENU:
             self.close()
         
 def main():
